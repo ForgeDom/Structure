00e31f7c4ad7a4d5b3d7a7bbe6d250f7af6f8784
Max <maxstill2006@gmail.com>
2024-07-05 22:12:18 +0300

    +

--- main.cpp
+++ main.cpp
@@ -1,7 +1,26 @@
 #include <iostream>
+#include <cmath>
+
 using namespace std;
 
-int main() {
+struct Point {
+    double x;
+    double y;
+};
 
-	return 0;
+double distance(const Point& p1, const Point& p2) {
+    double dx = p2.x - p1.x;
+    double dy = p2.y - p1.y;
+    return sqrt(dx * dx + dy * dy);
+}
+
+int main() {
+    Point point1 = { 0.0, 0.0 };
+    Point point2 = { 3.0, 4.0 };
+
+    double dist = distance(point1, point2);
+
+    cout << "Distance between points: " << dist << endl;
+
+    return 0;
 }
